0a5a1e4c927c2f21897defd7f01c0e8f7bc2ef8e
Ari Weiland <math1man@gmail.com>
2016-04-06 00:21:19 -0500

    Converted everything from CRLF to LF Added a nice Logger class that keeps a list of string lines, and simultaneously prints them out to the console. Contains a super easy writeLogFile method. MatrixConstructor now uses a Logger class for all its intermediate logging and writes it out to a log file. It also uses the Logger to write the matrix to a (separate) file Converted the matrix building algorithm to a significantly cleaner Queue-based system. Much fewer indices flying around

--- src/main/java/edu/macalester/mscs/network/MatrixConstructor.java
+++ src/main/java/edu/macalester/mscs/network/MatrixConstructor.java
@@ -1,20 +1,22 @@
 package edu.macalester.mscs.network;
 
 import edu.macalester.mscs.utils.FileUtils;
+import edu.macalester.mscs.utils.Logger;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 
 public class MatrixConstructor {
 
-	public static void main(String[] args){
-		printResultCSV(getData(CharacterLists.NEW_SHORT_LIST, getText("src/main/resources/text/got.txt")),
-				"src/main/resources/data/logs/GoT1-16-4-matrix2.csv");
-	}
-
 	public static final int NOISE = 4;
 	// threshold: 3=13
 	public static final int REACH = 20;
+
+	public static void main(String[] args){
+		printResultCSV(getData(CharacterLists.NEW_SHORT_LIST, getText("src/main/resources/text/got.txt"), REACH, NOISE,
+						"src/main/resources/data/logs/log.txt"),
+				"src/main/resources/data/logs/GoT1-16-4-matrix2.csv");
+	}
 
 	public static String getText(String file) {
 		List<String> lines = FileUtils.readFile(file);
@@ -25,7 +27,9 @@ public class MatrixConstructor {
 		return sb.toString();
 	}
 
-	public static MatrixAndNames getData(String characters, String text) {
+	public static MatrixAndNames getData(String characters, String text, int reach, int noise, String logFile) {
+
+		Logger logger = new Logger();
 
 		List<String> nameList = new ArrayList<>();
 		Map<String, Integer> nameIndices = new HashMap<>();
@@ -65,132 +69,126 @@ public class MatrixConstructor {
 
 		String[] input = text.split(" ");
 
-		System.out.println("=============================================================");
-		System.out.println("=================== PART 1: General Info ====================");
-		System.out.println("=============================================================");
-		System.out.println("Book: A Game of Thrones");
-		System.out.println("Character names input: " + characters);
-		System.out.println("Proximity check range: " + REACH);
-		System.out.println("Noise threshold level: " + NOISE);
-		System.out.println();
+		logger.log("=============================================================");
+		logger.log("=================== PART 1: General Info ====================");
+		logger.log("=============================================================");
+		logger.log();
+		logger.log("Character names input: " + characters);
+		logger.log("Proximity check range: " + reach);
+		logger.log("Noise threshold level: " + noise);
+		logger.log();
+		logger.log();
+		logger.log("=============================================================");
+		logger.log("================== PART 2: Edge Collection ==================");
+		logger.log("=============================================================");
 
+		// TODO: avoid multiple hits per encounter
+		// TODO: allow for multi-word names
 		List<Encounter> edgeWeights = new ArrayList<>();
-		for (int i=0; i < input.length; i++) { // iterate through text
-			for (String primary : nicknameRedirects.keySet()) { // iterate through names
-				if (isName(input[i], primary)) {
-					int m1 = nicknameRedirects.get(primary);
-//					Set<Integer> neighbors = new HashSet<>();
-					// search adjacent words in either direction
-					for (int j = i - 1; j >= Math.max(0, i - REACH); j--) {
-						for (String secondary : nicknameRedirects.keySet()) {
-							if (isName(input[j], secondary)) {
-								int m2 = nicknameRedirects.get(secondary);
-								if (m1 != m2) {
-									matrix[m1][m2]++;
-									matrix[m2][m1]++;
-//									neighbors.add(m2);
-
-									String context = "";
-									for (int contextInd = Math.min(i, j) - 5; contextInd < Math.max(i, j) + 5; contextInd++) {
-										try {
-											context += input[contextInd] + " ";
-										} catch (Exception ignored) {
-										}
-									}
-									edgeWeights.add(new Encounter(primary, secondary, i, j, context));
-								}
-							}
-						}
-					}
-//					for (int m2 : neighbors) {
-//						matrix[m1][m2]++;
-//						matrix[m2][m1]++;
-//					}
+		Queue<String> nameQueue = new ArrayDeque<>(reach + 1);
+		for (String chunk : input) { // iterate through text
+			// get the current name
+			String primary = "";
+			for (String name : nicknameRedirects.keySet()) {
+				if (isName(chunk, name)) {
+					primary = name;
 				}
 			}
-        }
+			// tally neighbors
+			if (!primary.isEmpty()) {
+				int index1 = nicknameRedirects.get(primary);
+				for (String secondary : nameQueue) {
+					if (!secondary.isEmpty()) {
+						int index2 = nicknameRedirects.get(secondary);
+						if (index1 != index2) {
+                            matrix[index1][index2]++;
+                            matrix[index2][index1]++;
+                            String context = "";
+                            for (int contextInd = Math.min(index1, index2) - 5; contextInd < Math.max(index1, index2) + 5; contextInd++) {
+                                try {
+                                    context += input[contextInd] + " ";
+                                } catch (Exception ignored) {
+                                }
+                            }
+                            edgeWeights.add(new Encounter(primary, secondary, index1, index2, context));
+                        }
+					}
+				}
+			}
+			// update the queue
+			nameQueue.add(primary);
+			if (nameQueue.size() > reach) { // limit its size
+				nameQueue.poll();
+			}
+		}
 
-		System.out.println();
-		System.out.println();
-		System.out.println("=============================================================");
-		System.out.println("================== PART 2: Edge Collection ==================");
-		System.out.println("=============================================================");
 		// This section is exclusively printing out stuff, has no actual code purpose
-		System.out.println();
-		System.out.println("============ By Character (including alt. names): ===========");
+		logger.log();
+		logger.log("============ By Character (including alt. names): ===========");
 
 		for (String name : nicknameRedirects.keySet()) {
-            System.out.println();
-            System.out.println(name + ":");
+			logger.log();
+            logger.log(name + ":");
             for (Encounter edgeWeight : edgeWeights) {
                 if (edgeWeight.character1.equals(name)) {
-                    System.out.println(edgeWeight.toString());
+					logger.log(edgeWeight.toString());
                 }
             }
             for (Encounter edgeWeight : edgeWeights) {
                 if (edgeWeight.character2.equals(name)) {
-                    System.out.println(edgeWeight.toString());
-                }
-            }
-        }
+					logger.log(edgeWeight.toString());
+				}
+			}
+		}
 
-		System.out.println();
-		System.out.println("============ By Proximity (including alt. names): ===========");
+		logger.log();
+		logger.log("============ By Proximity (including alt. names): ===========");
 
-		for (int i = REACH; i > 0; i--) {
-            System.out.println();
-            System.out.println("Proximity = " + i + "-word :");
+		for (int i = reach; i > 0; i--) {
+			logger.log();
+			logger.log("Proximity = " + i + "-word :");
             for (Encounter edgeWeight : edgeWeights) {
                 if (edgeWeight.proximity == i) {
-                    System.out.println(edgeWeight.toString());
-                }
-            }
-        }
+					logger.log(edgeWeight.toString());
+				}
+			}
+		}
 
-		System.out.println();
-		System.out.println();
-		System.out.println("=============================================================");
-		System.out.println("=================== PART 3: Refining Data ===================");
-		System.out.println("=============================================================");
-		System.out.println();
+		logger.log();
+		logger.log();
+		logger.log("=============================================================");
+		logger.log("=================== PART 3: Refining Data ===================");
+		logger.log("=============================================================");
+		logger.log();
 
 		MatrixAndNames data = new MatrixAndNames(matrix, names);
-		data.cleanNoise(NOISE);
+		data.cleanNoise(noise);
 
-		System.out.println();
-		System.out.println();
-		System.out.println("=============================================================");
-		System.out.println("=================== PART 4: Matrix as CSV ===================");
-		System.out.println("=============================================================");
-		System.out.println();
+		logger.log();
+		logger.log();
+		logger.log("=============================================================");
+		logger.log("========================= End of log ========================");
+		logger.log("=============================================================");
+		logger.log();
+		logger.writeLog(logFile);
 
-		printResultCSV(data, "src/main/resources/data/logs/GoT1-16-4-matrix.csv");
-
-		System.out.println();
-		System.out.println();
-		System.out.println("=============================================================");
-		System.out.println("========================= End of log ========================");
-		System.out.println("=============================================================");
 		return data;
     }
 
 	private static boolean isName(String input, String name) {
-		// this way accounts for non-letter characters, but ignores word containment
 		return input.matches("(.*\\W)*" + name + "(\\W.*)*");
-//		return input.contains(name); // the old way
 	}
 
 	private static void printResultCSV(MatrixAndNames data, String file) {
-		List<String> lines = new ArrayList<>();
-		String line = StringUtils.strip(Arrays.toString(data.getNames()), "[]").replaceAll(" ", "");
-		lines.add(line);
-		System.out.println(line);
-		for(int i=0; i<data.getMatrix().length; i++){
-			line = StringUtils.strip(Arrays.toString(data.getMatrix()[i]), "[]").replaceAll(" ", "");
-			lines.add(line);
-			System.out.println(line);
+		Logger logger = new Logger();
+		logger.log(cleanArrayString(Arrays.toString(data.getNames())));
+		for (int[] row : data.getMatrix()) {
+			logger.log(cleanArrayString(Arrays.toString(row)));
 		}
-		FileUtils.writeFile(lines, file);
+		logger.writeLog(file);
 	}
 
+	private static String cleanArrayString(String arrayString) {
+		return StringUtils.strip(arrayString, "[]").replaceAll(" ", "");
+	}
 }
---
+++ src/main/java/edu/macalester/mscs/utils/Logger.java
@@ -0,0 +1,38 @@
+package edu.macalester.mscs.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ari Weiland
+ */
+public class Logger {
+
+    private final List<String> log;
+
+    public Logger() {
+        this(new ArrayList<String>());
+    }
+
+    public Logger(List<String> log) {
+        this.log = log;
+    }
+
+    public void log() {
+        log(null);
+    }
+
+    public void log(Object o) {
+        if (o == null) {
+            log.add("");
+            System.out.println();
+        } else {
+            log.add(o.toString());
+            System.out.println(o.toString());
+        }
+    }
+
+    public void writeLog(String file) {
+        FileUtils.writeFile(log, file);
+    }
+}
