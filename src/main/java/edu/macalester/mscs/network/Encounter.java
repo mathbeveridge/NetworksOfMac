c230acbdcb89ce9755fd58f95aa7e168d239ce8e
Ari Weiland <math1man@gmail.com>
2016-04-06 17:24:41 -0500

    Deleted a piontless thing

--- src/main/java/edu/macalester/mscs/network/Encounter.java
+++ src/main/java/edu/macalester/mscs/network/Encounter.java
@@ -4,23 +4,26 @@ public class Encounter {
 	
 	public final String character1;
 	public final String character2;
-	public final int cIndex1;
-	public final int cIndex2;
 	public final int proximity;
 	public final String context;
-	
-	public Encounter(String character1, String character2, int cIndex1, int cIndex2, String context) {
+
+	public Encounter(String character1, String character2, String context) {
+		this(character1, character2, -1, context);
+	}
+
+	public Encounter(String character1, String character2, int proximity, String context) {
 		this.character1 = character1;
 		this.character2 = character2;
-		this.cIndex1 = cIndex1;
-		this.cIndex2 = cIndex2;
-		this.proximity = Math.abs(cIndex1 - cIndex2);
+		this.proximity = proximity;
 		this.context = context;
 	}
-	
+
 	public String toString() {
-		return this.character1 + ", " + this.character2 + ", " 
-				+ this.cIndex1 + ", " + this.cIndex2 + ", " + this.proximity + ", " + this.context;
+		if (proximity > -1) {
+			return this.character1 + ", " + this.character2 + ", " + this.proximity + ", \"" + this.context + "\"";
+		} else {
+			return this.character1 + ", " + this.character2 + ", \"" + this.context + "\"";
+		}
 	}
 
 }
