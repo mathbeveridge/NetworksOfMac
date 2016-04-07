ece7add14e1d8f9a4f08a11779e0e3214b8ab0fb
Ari Weiland <math1man@gmail.com>
2016-04-07 14:34:02 -0500

    Made encounter comparable. It first compares by position, then character1, then character2 Updated matrices to remove floaters

--- src/main/java/edu/macalester/mscs/network/Encounter.java
+++ src/main/java/edu/macalester/mscs/network/Encounter.java
@@ -1,6 +1,6 @@
 package edu.macalester.mscs.network;
 
-public class Encounter {
+public class Encounter implements Comparable<Encounter> {
 	
 	public final String character1;
 	public final String character2;
@@ -14,8 +14,21 @@ public class Encounter {
 		this.context = context;
 	}
 
+	@Override
+	public int compareTo(Encounter o) {
+		int posDif = position - o.position;
+		int c1dif = character1.compareTo(o.character1);
+		if (posDif != 0) {
+			return posDif;
+		} else if (c1dif != 0) {
+			return c1dif;
+		} else {
+			return character2.compareTo(o.character2);
+		}
+	}
+
+	@Override
 	public String toString() {
 		return this.character1 + ", " + this.character2 + ", " + this.position + ", \"" + this.context + "\"";
 	}
-
 }
