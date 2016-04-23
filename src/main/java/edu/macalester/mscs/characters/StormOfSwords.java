ebb8a2e3020a2570408ef9cd12ff356f5b3a8e74
Ari Weiland <math1man@gmail.com>
2016-04-22 20:41:26 -0500

    Fixed a mistake in DwD Starting on SoS

--- src/main/java/edu/macalester/mscs/characters/StormOfSwords.java
+++ src/main/java/edu/macalester/mscs/characters/StormOfSwords.java
@@ -10,7 +10,7 @@ import java.util.Set;
 /**
  * @author Ari Weiland
  */
-public class DanceWithDragons {
+public class StormOfSwords {
 
     public static final Set<String> IGNORED_WORDS = new HashSet<>(Arrays.asList(
             "My", "She", "He", "His", "Her", "We", "They", "Their", "You", "Your", "It", // pronouns
@@ -18,9 +18,7 @@ public class DanceWithDragons {
             "Who", "Why", "What", "Will", // questions
             "House", "Houses", "Clan", "Lords", "Ladies", "Kings", "Dothraki", "Grace", "God", // GoT specific
             "Father", "Mother", "Uncle", "Aunt", "Brother", "Brothers", "Sons", "Daughter", "Cousin", // familial references
-            "Men", "Man", "And", "Sleepy", "Worship", "Magnificence", "Planky", "Town", "Beyond",
-            "Dornish", "Wedding", "Common", "Velvet", "Noble", "Broken", "Star", "Tongue", "Took",
-            "Glass", "Winter"//, "Queer" // miscellaneous
+            "Men", "Man", "And", "With", "Griffin", "No" // miscellaneous
     ));
 
     // Words that are not unique, but may still be descriptive, expecially in combination
@@ -45,8 +43,7 @@ public class DanceWithDragons {
             "Cruel", "Bold", "Brave", "Good", "Strong", "Bitter", "Sweet", "Bad", "Clever", "Cautious",
             "Wise", "Craven", "Poor", "Pretty", "Scared", "Homeless", "Hot", "Shy", "True", "Mad", "Blessed",
             "Queer", "Sour", "Cunning", "Hairy", // adjective titles
-            "Bear", "Iron", "Beggar", "Whore", "Wench", "Grandfather", "Water", "Crow", "Wolf", "Shepherd",
-            "Dance", "Butcher" // miscellaneous
+            "Conqueror" // miscellaneous
     ));
 
     // Words that are sometimes placed between first and last names
@@ -56,24 +53,12 @@ public class DanceWithDragons {
         // initialize the finder
         CharacterFinder finder = new CharacterFinder(IGNORED_WORDS, TITLE_WORDS, GENERAL_WORDS, FILLER_WORDS, ".?!�");
         // read in the text
-        finder.countCapitalized(FileUtils.readFile("src/main/resources/text/dancewithdragons.txt"));
+        finder.countCapitalized(FileUtils.readFile("src/main/resources/text/stormofswords.txt"));
         // fix a few mistakes
-        finder.incrementName("Petyr Baelish", 2);
-        finder.incrementName("Petyr", 0);
-        finder.incrementName("Jeor Mormont", 0);
-        finder.removeWords("Reek Ramsay");
-        finder.removeWords("Summer Islander King Robert");
-        finder.removeWords("Pyke Lord Dagon");
-        finder.removeWords("Jon of Hodor");
-        finder.removeWords("Rhodry Lord Cerwyn");
-        finder.removeWords("Henly Lord Slate");
 
         // gather names, titles, places, and things
         Set<String> titledNames = finder.getTitledNames();
         Set<String> pluralizedNames = finder.getPluralizedNames();
-        pluralizedNames.remove("Walder");
-        pluralizedNames.remove("Cat");
-        pluralizedNames.remove("Aegon");
         Set<String> surnames = finder.getSurnames();
         Set<String> names = finder.getNamesBySurname(surnames);
         names.addAll(titledNames);
@@ -93,7 +78,7 @@ public class DanceWithDragons {
         finder.removeTitles();
         finder.removeWordsBelowThreshold(lonely, 1);
 
-        finder.printCounter().writeLog("src/main/resources/data/characters/dwd-counter.csv");
+        finder.printCounter().writeLog("src/main/resources/data/characters/sos-counter.csv");
 
         // gather phrases that are not inherently descriptive
         Set<String> nondescriptors = new HashSet<>();
@@ -102,17 +87,14 @@ public class DanceWithDragons {
         nondescriptors.addAll(surnames);
         nondescriptors.addAll(WordUtils.getPlurals(surnames));
         nondescriptors.addAll(places);
-        nondescriptors.add("Qarl");
-        nondescriptors.add("Jack");
-        nondescriptors.add("Ralf");
 
         // build character groups
         finder.buildCharacterGroups(nondescriptors);
 
         // manually combine more character groups
-        finder.combineGroups("Robert Baratheon", "Usurper");
         finder.combineGroups("Eddard", "Ned");
-        finder.combineGroups("Jon", "Lord Snow", "Lord Crow");
+        finder.combineGroups("Jon", "Lord Snow");
+        finder.combineGroups("Catelyn", "Cat");
         finder.combineGroups("Bran", "Brandon Stark");
         finder.combineGroups("Petyr", "Littlefinger");
         finder.combineGroups("Daenerys", "Dany", "Khaleesi");
@@ -122,59 +104,31 @@ public class DanceWithDragons {
         finder.combineGroups("Jeor Mormont", "Old Bear", "Commander Mormont", "Lord Mormont");
         finder.combineGroups("Pycelle", "Grand Maester");
         finder.combineGroups("Lysa", "Lady Arryn", "Lady of the Eyrie");
-        finder.combineGroups("Tywin", "Lord of Casterly Rock", "Lord Lannister");
+        finder.combineGroups("Tywin", "Lord of Casterly Rock");
         finder.combineGroups("Roose", "Lord of the Dreadfort", "Lord Bolton");
         finder.combineGroups("Loras", "Knight of Flowers");
-        finder.combineGroups("Davos", "Onion Knight", "Lord Seaworth");
+        finder.combineGroups("Davos", "Onion Knight");
         finder.combineGroups("Varys", "Spider");
         finder.combineGroups("Gregor", "Mountain");
-        finder.combineGroups("Balon Greyjoy", "Reaper", "Lord of the Iron Islands", "Lord of Pyke");
-        finder.combineGroups("Theon", "Reek");
         finder.combineGroups("Cersei", "Queen Regent");
         finder.combineGroups("Rattleshirt", "Lord of Bones");
-        finder.combineGroups("Wyman", "Lord of White Harbor", "Lord Lard", "Lord Manderly");
         finder.combineGroups("Randyll", "Lord Tarly");
         finder.combineGroups("Mace", "Lord Tyrell");
         finder.combineGroups("Stannis", "Lord of Dragonstone");
         finder.combineGroups("Rickard Karstark", "Lord Karstark");
         finder.combineGroups("Janos", "Lord Slynt");
-        finder.combineGroups("Quentyn", "Frog");
-        finder.combineGroups("Rolly", "Duck");
-        finder.combineGroups("Jon Connington", "Griff", "Lord Connington");
-        finder.combineGroups("Tyrion", "Imp", "Hugor", "Yollo");
+        finder.combineGroups("Jon Connington", "Lord Connington");
+        finder.combineGroups("Tyrion", "Imp");
         finder.combineGroups("Hother", "Whoresbane");
-        finder.combineGroups("Nymeria", "Nym");
-        finder.combineGroups("Barristan", "Ser Grandfather");
-        finder.combineGroups("Ghazdor", "Wobblecheeks");
-        finder.combineGroups("Arya", "Arry", "Cat");
-        finder.combineGroups("Morgan", "Middle Liddle");
-        finder.combineGroups("Yezzan", "Yellowbelly");
+        finder.combineGroups("Arya", "Arry");
         finder.combineGroups("Aerys", "Mad King");
-        finder.combineGroups("Tattered Prince", "Tatters", "Rags");
-        finder.combineGroups("Mance Rayder", "Abel");
-        finder.combineGroups("Barbrey", "Lady Dustin", "Lady of Barrowton");
         finder.combineGroups("Ben Plumm", "Brown Ben");
-        finder.combineGroups("Varamyr", "Lump");
         finder.combineGroups("Salladhor", "Salla");
-        finder.combineGroups("Aegor", "Bittersteel");
-        finder.combineGroups("Cleon", "Butcher King");
-        finder.combineGroups("Tytos Blackwood", "Lord of Raventree");
-        finder.combineGroups("Godric", "Lord Borrell", "Lord of Sweetsister");
-        finder.combineGroups("Harwood Stout", "Lord Stout");
         finder.combineGroups("Paxter", "Lord Redwyne");
-        finder.combineGroups("Rodrik Ryswell", "Lord Ryswell");
-        finder.combineGroups("Brandon Norrey", "Lord Norrey");
         finder.combineGroups("Jonos", "Lord Bracken");
         finder.combineGroups("Tytos Blackwood", "Lord Blackwood");
-        finder.combineGroups("Lord Hornwood", "Lord of the Hornwood");
-        finder.combineGroups("Willam", "Lord Dustin");
-        finder.combineGroups("Shrouded Lord", "Prince of Sorrows");
-        finder.combineGroups("Sansa", "Lady Lannister");
-        finder.combineGroups("Sybelle", "Lady Glover");
-        finder.combineGroups("Melisandre", "Lady Red");
 
         // manually add important names that get missed
-        names.add("Ned Woods");
         names.add("Ben Bones");
         names.add("Mance Rayder");
         names.add("Hodor");
@@ -321,10 +275,10 @@ public class DanceWithDragons {
 
         Set<String> firstNames = finder.getFirstNames(names);
 
-        FileUtils.writeFile(finder.getNameList(), "src/main/resources/data/characters/dwd-list-full.txt");
-        FileUtils.writeFile(finder.getNameList(names), "src/main/resources/data/characters/dwd-list-clean.txt");
-        FileUtils.writeFile(finder.getNameList(firstNames), "src/main/resources/data/characters/dwd-list-no-dup.txt");
-        FileUtils.writeFile(finder.getFirstNameList(names), "src/main/resources/data/characters/dwd-list-first.txt");
+        FileUtils.writeFile(finder.getNameList(), "src/main/resources/data/characters/sos-list-full.txt");
+        FileUtils.writeFile(finder.getNameList(names), "src/main/resources/data/characters/sos-list-clean.txt");
+        FileUtils.writeFile(finder.getNameList(firstNames), "src/main/resources/data/characters/sos-list-no-dup.txt");
+        FileUtils.writeFile(finder.getFirstNameList(names), "src/main/resources/data/characters/sos-list-first.txt");
 
     }
 }
